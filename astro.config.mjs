ce70e3da80688a47130559ab722b0297be39af94
iprobablydontknowwhatimdoing <octact82@gmail.com>
2026-05-21 10:23:46 -0700

    try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
 		cssVariable: "--atk-mono",
 	}],
 	site: 'https://iprobablydontknowwhatimdoing.github.io',
+	base: '/english-project',
 	build: {
 		inlineStylesheets: 'always',
 	}
